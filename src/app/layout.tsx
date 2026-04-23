a93063afcfb8ca47e93ad694aaa51582a81ec96b
taha-ahmed-hodhod <121146428+taha-ahmed-hodhod@users.noreply.github.com>
2026-04-23 18:35:29 +0200

    feature/ create navbar
    * feature/ create navbar
    
    * fix bug in navbar component
    
    ---------
    
    Co-authored-by: honda4coding <123742038+honda4coding@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar/Navbar";
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
 import Footer from "@/components/footer/Footer";
 
 const geistSans = Geist({
@@ -29,6 +33,8 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <Navbar />
+        {children}
         <main className="flex-1">{children}</main>
         <Footer />
       </body>
